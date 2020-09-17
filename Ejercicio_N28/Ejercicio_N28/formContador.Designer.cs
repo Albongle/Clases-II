aa3ce954fa32f276900cb0bc681ad4546ba1fcc9
Albongle <alejandro.bongioanni@gmail.com>
2020-09-17 19:01:47 -0300

    Ejercicio 28

---
+++ Ejercicio_N28/Ejercicio_N28/formContador.Designer.cs
@@ -0,0 +1,76 @@
+﻿namespace Ejercicio_N28
+{
+    partial class frmContador
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.rtbCajaDetexto = new System.Windows.Forms.RichTextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // rtbCajaDetexto
+            // 
+            this.rtbCajaDetexto.Location = new System.Drawing.Point(-1, -1);
+            this.rtbCajaDetexto.Name = "rtbCajaDetexto";
+            this.rtbCajaDetexto.Size = new System.Drawing.Size(451, 407);
+            this.rtbCajaDetexto.TabIndex = 0;
+            this.rtbCajaDetexto.Text = "";
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(342, 423);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(96, 24);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Calcular";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // frmContador
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(451, 450);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rtbCajaDetexto);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmContador";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Contador de Palabras";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox rtbCajaDetexto;
+        private System.Windows.Forms.Button btnCalcular;
+    }
+}
+
---
+++ Ejercicio_N28/Ejercicio_N28/formContador.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Ejercicio_N28
+{
+    public partial class frmContador : Form
+    {
+        public frmContador()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            string[] palabrasAux = new string[(this.rtbCajaDetexto.Text.Split(' ')).Length]; // declaro un array de string del tamaño de arrays que puedo generar desde el ingreso del RichText
+            palabrasAux = this.rtbCajaDetexto.Text.Split(' '); // le cargo el texto ingresado en forma de array
+            Dictionary<string, int> diccionario= new Dictionary<string, int>();
+            List<KeyValuePair<string, int>> listaAuxiliar; // declaro una lista de tipo keyvaluepair en donde voy a guardar la lista diccionario
+            foreach (string palabra in palabrasAux)
+            {              
+                if(!(diccionario.ContainsKey(palabra.ToUpper())) && !(string.IsNullOrEmpty(palabra))) // me fijo que la palabra este no este en la lista y esta no sea null o vacia
+                {
+                    diccionario.Add(palabra.ToUpper(), 1);
+                }
+                else if (!(string.IsNullOrEmpty(palabra)))
+                {
+                    diccionario[palabra.ToUpper()] = diccionario[palabra.ToUpper()] + 1;
+                }
+            }
+            listaAuxiliar = diccionario.ToList();
+            listaAuxiliar.Sort(FuncionCriterio);
+            for (int i = 0; i < 4; i++)
+            {
+                MessageBox.Show("La palabra " + listaAuxiliar[i].Key + " se repitio " + listaAuxiliar[i].Value, "Palabra", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+        }
+        private int FuncionCriterio (KeyValuePair<string, int> primerValor, KeyValuePair<string, int> segundoValor)
+        {
+            int returnAux = 0;
+            if(primerValor.Value> segundoValor.Value)
+            {
+                returnAux = -1;
+            }
+            else if (primerValor.Value < segundoValor.Value)
+            {
+                returnAux = 1;
+            }
+            return returnAux;
+        }
+
+    }
+}
