d6dfdfcc7adff6a3ae650f67bdf0e4b4b6fd9bf1
Albongle <alejandro.bongioanni@gmail.com>
2020-09-18 17:47:17 -0300

    Ejercicio 30

---
+++ Ejercicio_N30/Ejercicio_N30/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca;
+
+namespace Ejercicio_N30
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random azar = new Random();
+            Competencia formulaUno = new Competencia(10, 5);
+            AutoF1[] autos = new AutoF1[10];
+
+            for (int i = 0; i < autos.Length; i++)
+            {
+                autos[i] = new AutoF1((short)i, azar.Next(1, 5).ToString());
+                if ((formulaUno + autos[i]) == true)
+                {
+                    Console.WriteLine("Se Agrego el siguiente auto a la competencia: {0}", autos[i].MostrarDatos());
+                }
+                else
+                {
+                    Console.WriteLine("No se agrego ya que la competencia esta completa: {0}", autos[i].MostrarDatos());
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
