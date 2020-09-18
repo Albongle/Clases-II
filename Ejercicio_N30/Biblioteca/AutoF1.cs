bd97a37fc6eb87172187c75429958c308482ccf0
Albongle <alejandro.bongioanni@gmail.com>
2020-09-18 18:08:54 -0300

    Ejercicio 30

---
+++ Ejercicio_N30/Biblioteca/AutoF1.cs
@@ -0,0 +1,62 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class AutoF1
+    {
+        private short cantidadCombustible;
+        private bool enCompetencia;
+        private string escuderia;
+        private short numero;
+        private short vueltasRestantes;
+
+        public AutoF1(short numero, string escuderia)
+        {
+            this.numero = numero;
+            this.escuderia = escuderia;
+            this.cantidadCombustible = 0;
+            this.vueltasRestantes = 0;
+        }
+        public string MostrarDatos()
+        {
+            return "El auto numero: " + this.numero + "\nEscuderia: " + this.escuderia + "\nEn competencia: " + this.enCompetencia + "\nCantidad de combustible: " + this.cantidadCombustible + "\nVueltas restantes: " + this.vueltasRestantes;
+        }
+        public static bool operator ==(AutoF1 a1, AutoF1 a2)
+        {
+            return a1.escuderia == a2.escuderia;
+        }
+        public static bool operator !=(AutoF1 a1, AutoF1 a2)
+        {
+            return !(a1 == a2);
+        }
+        public short GetCombustible()
+        {
+            return this.cantidadCombustible;
+        }
+        public void SetCombustible(short cantidadCombustible)
+        {
+           this.cantidadCombustible=cantidadCombustible;
+        }
+        public bool GetEnCompetencia()
+        {
+            return this.enCompetencia;
+        }
+        public void SetEnCompetencia(bool enCompetencia)
+        {
+            this.enCompetencia = enCompetencia;
+        }
+        public short GetVueltasRestantes()
+        {
+            return this.vueltasRestantes;
+        }
+        public void SetVueltasRestantes(short vueltasRestantes)
+        {
+            this.vueltasRestantes = vueltasRestantes;
+        }
+
+    }
+}
---
+++ Ejercicio_N30/Biblioteca/Competencia.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Competencia
+    {
+        private short cantidadCompetidores;
+        private short cantidadVueltas;
+        private List<AutoF1> competidores;
+        private Competencia()
+        {
+            competidores = new List<AutoF1>();
+        }
+        public Competencia(short cantidadVueltas, short cantidadCompetidores) : this ()
+        {
+            this.cantidadVueltas = cantidadVueltas;
+            this.cantidadCompetidores = cantidadCompetidores;
+        }
+        public static bool operator +(Competencia c, AutoF1 a)
+        {
+            Random combusRandom = new Random();
+            bool returnAux = false;
+            if (c.competidores.Count() < c.cantidadCompetidores && (c != a))
+            {
+                a.SetEnCompetencia(true);
+                a.SetVueltasRestantes(c.cantidadVueltas);
+                a.SetCombustible((short)combusRandom.Next(15, 100));
+                c.competidores.Add(a);
+                returnAux = true;
+            }
+            return returnAux;
+        }
+        public static bool operator -(Competencia c, AutoF1 a)
+        {
+            bool returnAux = false;
+            if (c.competidores.Count() > 0 && (c == a))
+            {
+                c.competidores.Remove(a);
+                returnAux = true;
+            }
+            return returnAux;
+        }
+        public static bool operator ==(Competencia c, AutoF1 a)
+        {
+            return c.competidores.Contains(a);
+        }
+        public static bool operator !=(Competencia c, AutoF1 a)
+        {
+            return !(c == a);
+        }
+    }
+}
+
