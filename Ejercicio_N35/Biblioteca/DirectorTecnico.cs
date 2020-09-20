87303bad5b0758fc13335165ac300dceed56ed1f
Albongle <alejandro.bongioanni@gmail.com>
2020-09-20 00:06:34 -0300

    Ejercicio 35

---
+++ Ejercicio_N35/Biblioteca/DirectorTecnico.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class DirectorTecnico :Persona
+    {
+        private DateTime fechaNaciemiento;
+        private DirectorTecnico(string nombre)
+            :base(nombre)
+        {
+
+        }
+        public DirectorTecnico(string nombre, DateTime fechaNaciemiento) :this(nombre)
+        {
+            this.fechaNaciemiento = fechaNaciemiento;
+        }        
+        public static bool operator ==(DirectorTecnico dt1, DirectorTecnico dt2)
+        {
+            return (dt1.Nombre == dt2.Nombre && dt1.fechaNaciemiento == dt2.fechaNaciemiento);
+        }
+        public static bool operator !=(DirectorTecnico dt1, DirectorTecnico dt2)
+        {
+            return !(dt1 == dt2);
+        }
+        public string MostrarDatos()
+        {
+            return "Nombre DT: " + this.Nombre
+                + "\nFecha de Nacimiento: " + this.fechaNaciemiento;
+          }
+    }
+}
---
+++ Ejercicio_N35/Biblioteca/Jugador.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class Jugador : Persona
+    {
+        private int partidosJugados;
+        private int totalGoles;
+
+        public Jugador(int dni, string nombre)
+            :base(dni,nombre)
+        {
+            this.partidosJugados = 0;
+            this.totalGoles = 0;
+        }
+        public Jugador(int dni, string nombre, int totalGoles, int totalPartidos) : this(dni, nombre)
+        {
+            this.totalGoles = totalGoles;
+            this.partidosJugados = totalPartidos;
+        }
+        public float PromedioDeGoles
+        {
+            get
+            {
+                if (this.partidosJugados > 0)
+                {
+                    return this.TotalGoles / (float)this.PartidosJugados;
+                }
+                else
+                {
+                    return 0;
+                }
+            }
+        }
+        public int PartidosJugados
+        {
+            get
+            {
+                return this.partidosJugados;
+            }
+        }
+        public int TotalGoles
+        {
+            get
+            {
+                return this.totalGoles;
+            }
+        }
+        public static bool operator ==(Jugador j1, Jugador j2)
+        {
+            return j1.dni == j2.dni;
+        }
+        public static bool operator !=(Jugador j1, Jugador j2)
+        {
+            return !(j1 == j2);
+        }
+        public string MostrarDatos()
+        {
+            return "Jugador: " + this.Nombre
+                + "\nDNI: " + this.Dni
+                + "\nPartidos Jugados: " + this.partidosJugados 
+                + "\nTotal Goles: " + this.totalGoles 
+                + "\nPromedio Goles: " + this.PromedioDeGoles;
+        }
+
+    }
+}
