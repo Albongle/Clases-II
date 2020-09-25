cbda0c76f22521e84a16a71820b438455fe6fe55
Albongle <alejandro.bongioanni@gmail.com>
2020-09-24 21:30:16 -0300

    Central Telefonica

---
+++ CentralTelefonica/CentralitaHerencia/Llamada.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Llamada
+    {
+        protected float duracion;
+        protected string nroDestino;
+        protected string nroOrigen;
+        public enum TipoLlamada {Local,Provincial,Todas};
+        public Llamada(float duracion, string nroDestino, string nroOrigen)
+        {
+            this.duracion = duracion;
+            this.nroDestino=nroDestino;
+            this.nroOrigen = nroOrigen;
+        }
+        public float Duracion
+        {
+            get
+            {
+                return this.duracion;
+            }
+        }
+        public string NroDestino
+        {
+            get
+            {
+                return this.nroDestino;
+            }
+        }
+        public string NroOrigen
+        {
+            get
+            {
+                return this.nroOrigen;
+            }
+        }
+        public static int OrdernarPorDuracion(Llamada llamada1, Llamada llamada2)
+        {
+            int returnAux = 0;
+            if(llamada1.duracion<llamada2.duracion)
+            {
+                returnAux = -1;
+            }
+            else if(llamada1.duracion>llamada2.duracion)
+            {
+                returnAux = 1;
+            }
+            return returnAux;
+        }
+        public string Mostrar()
+        {
+            StringBuilder returnAux = new StringBuilder();
+            returnAux.AppendLine("La duracion de la llamada es: " + this.duracion);
+            returnAux.AppendLine("El numero de origen de la llamada es: " + this.nroOrigen);
+            returnAux.AppendLine("El numero de destino de la llamada es: " + this.nroDestino);
+            return returnAux.ToString();
+        }
+    }
+}
---
+++ CentralTelefonica/CentralitaHerencia/Local.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CentralitaHerencia
+{
+    public class Local :Llamada
+    {
+        protected float costo;
+        public Local (Llamada llamada,float costo)
+            :base(llamada.Duracion, llamada.NroDestino,llamada.NroOrigen)
+        {
+            this.costo = costo;
+        }
+        public Local(string origen, float duracion,string destino, float costo)
+            :base(duracion,destino,origen)
+        {
+            this.costo = costo;
+        }
+        public float CostoLlamada
+        {
+            get
+            {
+                return this.CalcularCosto();
+            }
+        }
+        public string Mostrar()
+        {
+            StringBuilder returnAux = new StringBuilder();
+            returnAux.AppendLine(base.Mostrar());
+            returnAux.AppendLine("El costo de la llamada local es: " + this.CostoLlamada);
+            return returnAux.ToString();
+        }
+        private float CalcularCosto()
+        {
+            return this.costo *this.duracion;
+        }
+    }
+}
