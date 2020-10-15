cd2633b5e2112e71e2dcf996c2fe866a9b516437
Albongle <alejandro.bongioanni@gmail.com>
2020-10-15 12:22:05 -0300

    Ejercicio 36

---
+++ Ejercicio_N42/Biblioteca/MiClase.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class MiClase
+    {
+        public MiClase()
+        {
+            try
+            {
+                MiClase.MiMetodoEstatico();
+            }
+            catch(DivideByZeroException ex)
+            {
+                throw ex;
+            }
+
+        }
+        public MiClase(int num1)
+        {
+            try
+            {
+                MiClase.MiMetodoEstatico();
+            }
+            catch (DivideByZeroException ex)
+            {
+                throw new UnaExcepcion("2 constructor", ex);
+            }
+
+        }
+
+
+        public static void MiMetodoEstatico()
+        {
+            throw new DivideByZeroException("Division por cero");
+        }
+    }
+}
