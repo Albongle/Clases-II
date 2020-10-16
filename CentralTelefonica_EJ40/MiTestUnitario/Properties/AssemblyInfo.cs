6e528feff674b981ccb39423a77d71133d2c21c0
Albongle <alejandro.bongioanni@gmail.com>
2020-10-15 21:39:00 -0300

    Ejercicios

---
+++ CentralTelefonica_EJ40/MiTestUnitario/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MiTestUnitario")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MiTestUnitario")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("ceb493c1-1e59-4ac4-b9ac-d5f9c5f993de")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ CentralTelefonica_EJ40/MiTestUnitario/UnitTestCentralita.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CentralitaHerencia;
+using System.Collections.Generic;
+
+namespace MiTestUnitario
+{
+    [TestClass]
+    public class UnitTestCentralita
+    {
+        [TestMethod]
+        public void ValidaListaDeLlamadas()
+        {
+            //Arrange
+            Centralita nuevaCentralita = new Centralita();
+            //Act
+            //Assert
+            Assert.IsNotNull(nuevaCentralita.Llamadas);          
+        }
+        [TestMethod]
+        [ExpectedException(typeof(CentralitaException))]
+        public void ValidaExcepcionLocal()
+        {
+            //Arrange
+            Centralita nuevaCentralita = new Centralita();
+            Local llamada = new Local("Bernal", 30, "Rosario", 2.65f);
+            //Act
+            nuevaCentralita += llamada;
+            nuevaCentralita += llamada;
+            //Assert            
+        }
+        [TestMethod]
+        [ExpectedException(typeof(CentralitaException))]
+        public void ValidaExcepcionProvincial()
+        {
+            //Arrange
+            Centralita nuevaCentralita = new Centralita();
+            Provincial llamada = new Provincial("Morón", Provincial.Franja.Franja_1, 21, "Bernal");
+            Provincial llamadaDos = new Provincial("Morón", Provincial.Franja.Franja_1, 21, "Bernal");
+            //Act
+            nuevaCentralita += llamada;
+            nuevaCentralita += llamadaDos;
+            //Assert            
+        }
+        [TestMethod]
+        public void ValidaTipoDeLlamado()
+        {
+            //Arrange
+            Local llamadaLocal1 = new Local("Bernal", 30, "Rosario", 2.65f);
+            Local llamadaLocal2 = new Local("Bernal", 30, "Rosario", 2.65f);
+            Provincial llamadaProv1 = new Provincial("Bernal", Provincial.Franja.Franja_1, 21, "Rosario");
+            Provincial llamadaProv2 = new Provincial("Bernal", Provincial.Franja.Franja_1, 21, "Rosario");
+            //Act
+            //Assert
+            Assert.AreEqual(llamadaLocal1, llamadaLocal2);
+            Assert.AreNotEqual(llamadaLocal1, llamadaProv1);
+            Assert.AreNotEqual(llamadaLocal1, llamadaProv2);
+            Assert.AreEqual(llamadaProv1, llamadaProv2);
+            Assert.AreNotEqual(llamadaLocal2, llamadaProv1);
+            Assert.AreNotEqual(llamadaLocal2, llamadaProv2);
+        }
+    }
+}
+
