65472562e496aef52ab61528388f53324843a6eb
Albongle <alejandro.bongioanni@gmail.com>
2020-10-19 10:55:45 -0300

    Ejercicio 45

---
+++ Ejercicio_N42/MiTestUnitario/MiTestUnitario.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Biblioteca;
+
+namespace MiTestUnitario
+{
+    [TestClass]
+    public class MiTestUnitario
+    {
+        [TestMethod]
+        [ExpectedException(typeof(MiExcepcion))]
+        public void TestMetodoInstancia()
+        {
+            //Arrange
+            OtraClase otra = new OtraClase();
+            //Act
+            otra.MetodoInstancia();
+            //Assert
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestMetodoStatico()
+        {
+            //Arrange
+            //Act
+            MiClase.MiMetodoEstatico();
+            //Assert
+        }
+        [TestMethod]
+        [ExpectedException(typeof(UnaExcepcion))]
+        public void TestConstructorMiClaseConParametro()
+        {
+            //Arrange
+            MiClase miclase = new MiClase(10);
+            //Act
+            //Assert
+        }
+        [TestMethod]
+        [ExpectedException(typeof(DivideByZeroException))]
+        public void TestConstructorMiClase()
+        {
+            //Arrange
+            MiClase miclase = new MiClase();
+            //Act
+            //Assert
+        }
+    }
+}
