61ebe01b4c856ea76e1c1055b333165a872d7cb5
Albongle <alejandro.bongioanni@gmail.com>
2020-10-22 17:57:04 -0300

    Ejercicio 49

---
+++ Ejercicio_N48/Ejercicio_N48/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Biblioteca;
+
+namespace Ejercicio_N48
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random random = new Random();
+            Factura[] factura = new Factura[10];
+            Recibo [] recibo = new Recibo[10];
+            Contabilidad<Factura,Recibo> contabilidad = new Contabilidad<Factura, Recibo>();
+
+            for (int i = 0; i < 10; i++)
+            {
+                factura[i] = new Factura(random.Next(0, 99999));
+                recibo[i] = new Recibo(random.Next(0, 99999));
+                contabilidad += factura[i];
+                contabilidad += recibo[i];
+            }
+
+            Console.WriteLine(contabilidad.ToString());
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Ejercicio_N49/Biblioteca/VehiculoDeCarrera.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Biblioteca
+{
+    public class VehiculoDeCarrera
+    {
+        private short cantidadCombustible;
+        private bool enCompetencia;
+        private string escuderia;
+        private short numero;
+        private short vueltasRestantes;
+        public VehiculoDeCarrera(short numero, string escuderia)
+        {
+            this.numero = numero;
+            this.escuderia = escuderia;
+        }
+        public short CantidadCombustible
+        {
+            get
+            {
+                return this.cantidadCombustible;
+            }
+            set
+            {
+                this.cantidadCombustible = value;
+            }
+        }
+        public bool EnCompetencia
+        {
+            get
+            {
+                return this.enCompetencia;
+            }
+            set
+            {
+                this.enCompetencia = value;
+            }
+        }
+        public string Escuderia
+        {
+            get
+            {
+                return this.escuderia;
+            }
+            set
+            {
+                this.escuderia = value;
+            }
+        }
+        public short Numero
+        {
+            get
+            {
+                return this.numero;
+            }
+            set
+            {
+                this.numero = value;
+            }
+        }
+        public short VueltasRestantes
+        {
+            get
+            {
+                return this.vueltasRestantes;
+            }
+            set
+            {
+                this.vueltasRestantes = value;
+            }
+        }
+        public static bool operator == (VehiculoDeCarrera v1, VehiculoDeCarrera v2)
+        {
+            return (v1.Escuderia == v2.Escuderia) && (v1.Numero == v2.Numero);
+        }
+        public static bool operator !=(VehiculoDeCarrera v1, VehiculoDeCarrera v2)
+        {
+            return !(v1==v2);
+        }
+        public virtual string MostrarDatos()
+        {
+            return "El Vehiculo numero: " + this.Numero
+            + "\nEscuderia: " + this.Escuderia
+            + "\nEn competencia: " + this.EnCompetencia
+            + "\nCantidad de combustible: " + this.CantidadCombustible
+            + "\nVueltas restantes: " + this.VueltasRestantes;
+        }
+
+
+    }
+}
